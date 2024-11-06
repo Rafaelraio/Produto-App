8fe6fda2a72ccfb8654e82bd9bc826a4100dc43b
Rafael Augusto de Souza <rafaelaugustodesouza@MacBook-Air-de-Rafael.local>
2024-11-06 20:00:59 -0300

    Criação do aplicativo flutter conforme as especificações e o readme

---
+++ produto_app/lib/HTTP/produto.dart
@@ -0,0 +1,82 @@
+import 'dart:convert'; // Para converter a resposta em JSON
+import 'package:http/http.dart' as http;
+
+const api = "http://127.0.0.1:3000";
+
+Future<List<dynamic>> listProdutos() async {
+  final response = await http.get(Uri.parse('$api/produto'));
+
+  if (response.statusCode == 200) {
+    var lista = jsonDecode(response.body);
+    print(lista);
+    return lista;
+  } else {
+    throw Exception('Falha ao listar os produtos');
+  }
+}
+
+Future<dynamic> inserirProduto(desc, preco, estoque, data) async {
+  print(desc);
+  print(preco);
+  print(estoque);
+  print(data);
+  final response = await http.post(Uri.parse('$api/produto'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'descricao': '$desc',
+        'preco': '$preco',
+        'estoque': '$estoque',
+        'data': '$data'
+      }));
+}
+
+Future<dynamic> buscarProdutoPorId(id) async {
+  print(id);
+  final response = await http.get(Uri.parse('$api/produto/$id'));
+  if (response.statusCode == 200) {
+    var produto = jsonDecode(response.body);
+    print("testezudo");
+    print(produto);
+    return produto;
+  } else {
+    throw Exception('Falha ao buscar s produto');
+  }
+}
+
+Future<dynamic> atualizarProdutoPorId(id, desc, preco, estoque, data) async {
+  print(id);
+  final response = await http.put(Uri.parse('$api/produto/$id'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'descricao': '$desc',
+        'preco': '$preco',
+        'estoque': '$estoque',
+        'data': '$data'
+      }));
+
+  if (response.statusCode == 200) {
+    var produto = jsonDecode(response.body);
+    print("testezudo");
+    print(produto);
+    return produto;
+  } else {
+    throw Exception('Falha ao buscar s produto');
+  }
+}
+
+Future<dynamic> deletarProdutoPorId(id) async {
+  print(id);
+  final response = await http.delete(Uri.parse('$api/produto/$id'));
+  if (response.statusCode == 200) {
+    var produto = jsonDecode(response.body);
+    print("testezudo");
+    print(produto);
+    return produto;
+  } else {
+    throw Exception('Falha ao buscar s produto');
+  }
+}
---
+++ produto_app/lib/produto.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:produto_app/HTTP/produto.dart';
+
+class ProdutoNovo extends StatelessWidget {
+  ProdutoNovo({super.key});
+
+  final TextEditingController nomeController = TextEditingController();
+  final TextEditingController descController = TextEditingController();
+  final TextEditingController estoqueController = TextEditingController();
+  final TextEditingController dataController = TextEditingController();
+  final TextEditingController precoController = TextEditingController();
+
+  void _mostrarErro(BuildContext context, String mensagem) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Erro'),
+          content: Text(mensagem),
+          actions: [
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<bool> _mostrarConfirmar(BuildContext context, String mensagem) async {
+    bool option = (await showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Confirmação'),
+              content: Text(mensagem),
+              actions: [
+                TextButton(
+                  child: Text('Não '),
+                  onPressed: () {
+                    Navigator.of(context).pop(false);
+                  },
+                ),
+                TextButton(
+                  child: Text('Sim'),
+                  onPressed: () {
+                    Navigator.of(context).pop(true);
+                  },
+                ),
+              ],
+            );
+          },
+        )) ??
+        false;
+    return option;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text("Novo Produto"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text("Desc: "),
+                Expanded(
+                    child: TextField(
+                  controller: descController,
+                ))
+              ],
+            ),
+            Row(
+              children: [
+                Text("Preço: "),
+                Expanded(
+                    child: TextField(
+                  controller: precoController,
+                  keyboardType: TextInputType.number,
+                ))
+              ],
+            ),
+            Row(
+              children: [
+                Text("Estoque: "),
+                Expanded(
+                    child: TextField(
+                  controller: estoqueController,
+                  keyboardType: TextInputType.number,
+                ))
+              ],
+            ),
+            Row(
+              children: [
+                Text("Data:"),
+                Expanded(
+                    child: TextField(
+                  controller: dataController,
+                  readOnly: true,
+                  onTap: () async {
+                    DateTime? data = await showDatePicker(
+                        context: context,
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime(2100),
+                        initialDate: DateTime.now());
+                    if (data != null) {
+                      String formattedDate =
+                          "${data.year}-${data.month}-${data.day}";
+                      dataController.text = formattedDate;
+                    }
+                  },
+                ))
+              ],
+            ),
+            Row(
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      nomeController.clear();
+                      descController.clear();
+                      estoqueController.clear();
+                      dataController.clear();
+                      Navigator.pop(context);
+                    },
+                    child: Text("Cancelar")),
+                ElevatedButton(
+                    onPressed: () async {
+                      String desc = descController.text;
+                      String preco = precoController.text;
+                      String estoque = estoqueController.text;
+                      String data = dataController.text;
+
+                      num? precoNum = num.tryParse(preco);
+                      num? estoqueNum = int.tryParse(estoque);
+
+                      if (desc == "" ||
+                          preco == "" ||
+                          estoque == "" ||
+                          data == "") {
+                        _mostrarErro(context, "Nenhum campo deve estar vazio");
+                      } else if (precoNum == null || estoqueNum == null) {
+                        _mostrarErro(
+                            context, "Preço e estoque devem ser numeros, sendo que estoque deve ser um numero inteiro");
+                      } else {
+                        bool x = await _mostrarConfirmar(context, "Inserir?");
+                        if (x == true) {
+                          print(desc);
+                          print(preco);
+                          print(desc);
+                          print(estoque);
+                          print(data);
+                          inserirProduto(desc, preco, estoque, data);
+                          nomeController.clear();
+                          descController.clear();
+                          precoController.clear();
+                          estoqueController.clear();
+                          dataController.clear();
+                        }
+                        Navigator.pop(context, true);
+                      }
+                    },
+                    child: Text("Inserir"))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
